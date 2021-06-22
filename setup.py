9aa6e518960c8f3db50ecf307c84abd321053223
mansukim1125 <mansukim1125@gmail.com>
2021-06-22 23:28:28 +0900

    build: add long description, projec url in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_dir = path.abspath(path.dirname(__name__))
+with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='JudgeInterface',
-    version='1.0.0',
+    version='1.0.1',
     description='A DB Interface for Judge',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='mansu kim',
     author_email='mansukim1125@gmail.com',
-    url='https://mansu.ga',
+    url='https://github.com/mansukim1125/JudgeInterface',
     license='MIT',
+    python_requires='>=3',
     packages=['JudgeInterface']
 )
