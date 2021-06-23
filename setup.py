74d4d81c54b8be4383069ce1e88f6025c1aeef36
mansukim1125 <mansukim1125@gmail.com>
2021-06-23 23:22:38 +0900

    build: package version to 1.0..5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='JudgeInterface',
-    version='1.0.4',
+    version='1.0.5',
     description='A DB Interface for Judge',
     long_description=long_description,
     long_description_content_type='text/markdown',
